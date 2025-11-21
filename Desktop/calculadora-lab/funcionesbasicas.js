9d95ea2a8b12418399e86429ff36a9ee4f38ecec
Gabriela Garay <gabrielagaraysan@gmail.com>
2025-11-20 20:24:59 -0600

    Configuración inicial: HTML y JS creados y vinculados

---
+++ Desktop/calculadora-lab/funcionesbasicas.js
@@ -0,0 +1,32 @@
+funcionesbasicas.js 
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + b;
+}
+// Función para restar dos números
+function restar(a, b) {
+    return a - b;
+}
+// Función para multiplicar dos números
+function multiplicar(a, b) {
+    return a * b;
+}
+// Función para dividir dos números
+function dividir(a, b) {
+    return a / b;
+} 
+
+
+// variable "resultado" con la funciones sumar, restar, multiplicar y dividir
+let resultado=sumar(5, 3);
+console.log("La suma es: " + resultado);
+
+resultado=restar(10, 4);
+console.log("La resta es: " + resultado);
+
+resultado=multiplicar(6, 7);
+console.log("La multiplicación es: " + resultado);
+
+resultado=dividir(20, 5);
+console.log("La división es: " + resultado);
+
